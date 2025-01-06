78f7d1f053effa04cd2066dd68d612f7f6ec8657
DeograciousAggrey <deograciousaggrey@gmail.com>
2025-01-06 23:19:45 +0300

    updated code files

--- safire-contract/commands/deploy-factory.sh
+++ safire-contract/commands/deploy-factory.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 source .env
 export RPC_URL=$BASE_SEPOLIA_RPC_URL
-export SCAN_URL=$BASE_SEPOLIA_SCAN_URL
+export EXPLORER_URL=$BASE_SEPOLIA_SCAN_URL
 export EXPLORER_API_KEY=$BASE_SEPOLIA_SCAN_API_KEY
 
 # Adjust the contract path in the script call
-forge script ../script/deploy/DeployFactory.s.sol:DeployFactory \
-  --rpc-url $RPC_URL \
-  --etherscan-api-key $EXPLORER_API_KEY \
-  --broadcast 
-  
+forge script ../script/deploy/DeployFactory.s.sol:DeployFactory --rpc-url $RPC_URL --verifier-url $EXPLORER_URL --etherscan-api-key $EXPLORER_API_KEY --broadcast --verify
 
